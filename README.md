cd5cf3e269174261791a8ac39b146715ab705719
dorupopac <popadoru98@gmail.com>
2021-09-18 15:10:33 +0300

    implemented functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+https://doru-pictureinpicture.netlify.app/
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('test');
+const video = document.getElementById('video');
+const btn = document.getElementById('button');
+let mediaStream;
+
+// Prompt to select media stream, pass to video element, then play
+const selectMediaStream = async () => {
+  try {
+    mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  } catch (err) {
+    console.error('Something went wrong:', err.message);
+  }
+};
+
+btn.addEventListener('click', async () => {
+  // If the mediaStream prompt was rejected, a click on the btn will prompt it again
+  if (!mediaStream) selectMediaStream();
+  else if (!document.pictureInPictureElement) {
+    btn.disabled = true;
+    await video.requestPictureInPicture();
+    btn.disabled = false;
+  } else if (document.pictureInPictureElement) {
+    document.exitPictureInPicture();
+    selectMediaStream();
+  }
+});
+
+// On Load
+selectMediaStream();
